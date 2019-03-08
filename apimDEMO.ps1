57fd9f12072fda05ac3d828b6c8006512b2a5cfe
Sven Malvik <sven.malvik@vipps.no>
2019-03-08 11:35:43 +0100

    Added outline

--- apimDEMO.ps1
+++ apimDEMO.ps1
@@ -10,9 +10,6 @@ Set-StrictMode -Version 3
 # Context
 $apimContext = New-AzApiManagementContext -ResourceGroupName "VIPPS-AT2-rg" -ServiceName "VIPPSAT2api04"
 
-# Deploy isCanaryDown property with pwsh.
-New-AzApiManagementProperty -Context $apimContext -PropertyId "isCanaryDown" -Name "isCanaryDown" -Value "true"
-
 # Deploy canary swagger api to apim with pwsh
 Import-AzApiManagementApi -Context $apimContext -SpecificationFormat "Swagger" -SpecificationPath "/Users/sma/git/k8s-app-conf/deploy/apim/canary/swagger.json" -Path "canary" -ApiId "canary" 
 
@@ -21,3 +18,6 @@ Set-AzApiManagementPolicy -Context $apimContext -ApiId "canary" -PolicyFilePath 
 
 # Deploy canary product with pwsh
 New-AzApiManagementProduct -Context $apimContext -ProductId "canary" -Title "Canary" -Description "Infrastructure test API" -SubscriptionRequired $True -State "published"
+
+# Optional: Deploy isCanaryDown property with pwsh.
+New-AzApiManagementProperty -Context $apimContext -PropertyId "isCanaryDown" -Name "isCanaryDown" -Value "true"
