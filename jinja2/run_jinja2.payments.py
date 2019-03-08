8f3414fcead34d6d4da1f8ae9c66b0821d8d4888
Sven Malvik <sven.malvik@vipps.no>
2019-03-08 23:05:44 +0100

    .

---
+++ jinja2/run_jinja2.payments.py
@@ -0,0 +1,10 @@
+import jinja2
+from jinja2 import Environment
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader, variable_start_string="@@", variable_end_string="@@")
+TEMPLATE_FILE = "payments.policy.1.xml"
+template = templateEnv.get_template(TEMPLATE_FILE)
+outputText = template.render() 
+
+print(outputText)
